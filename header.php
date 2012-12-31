bcd093b0b39f942f4c5b5711974a4fa817c41243
James Shakespeare <j@jshakespeare.com>
2012-12-31 14:29:32 +0000

    backgrounds and layout

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 </head>
 <body id="<?php echo $post->post_name; ?>">
 
-<div class="wrapper" id="header-wrapper">
+<div class="wrapper clearfix" id="header-wrapper">
     <header class="clearfix">
         <a id="header-logo" title="<?php bloginfo('title'); ?>" href="/"><img src="<?php bloginfo('template_url'); ?>/img/js-logo.png" alt="<?php bloginfo('title'); ?>" /></a>
         <nav id="header-nav">
@@ -51,4 +51,21 @@
             </ul>
         </nav>
     </header>
+    <?php if(is_front_page()): ?>
+    <div class="intro">
+        <p>I'm James Shakespeare. I design and build <a href="/work/">websites</a>, I write things in <a href="/posts/">English</a> and <a href="https://github.com/jshakes" target="_blank">computer-speak</a> and I'm an all-round jolly good <a href="/enquire/">chap</a>.</p>
+        <ul class="link-icons">
+            <li class="rss">
+                <a href="/feed/">RSS</a>
+            </li>
+            <li class="github">
+                <a href="https://github.com/jshakes" target="_blank">Github</a>
+            </li>
+            <li class="linkedin">
+                <a href="http://www.linkedin.com/profile/view?id=124506439" target="_blank">LinkedIn</a>
+            </li>
+        </ul>
+    </div>
+    <?php endif; ?>
+<!-- /#header-wrapper -->
 </div>
--- page-home.php
+++ page-home.php
@@ -7,20 +7,7 @@ if ( have_posts() ) : the_post();
     
     <div class="container clearfix">
         
-        <div class="intro">
-            <p>I'm James Shakespeare. I design and build <a href="/work/">websites</a>, I write things in <a href="/posts/">English</a> and <a href="https://github.com/jshakes" target="_blank">computer-speak</a> and I'm an all-round jolly good <a href="/enquire/">chap</a>.</p>
-            <ul class="link-icons">
-                <li class="rss">
-                    <a href="/feed/">RSS</a>
-                </li>
-                <li class="github">
-                    <a href="https://github.com/jshakes" target="_blank">Github</a>
-                </li>
-                <li class="linkedin">
-                    <a href="http://www.linkedin.com/profile/view?id=124506439" target="_blank">LinkedIn</a>
-                </li>
-            </ul>
-        </div>    
+            
         
         <div class="wrapper">
             <section class="s2">
