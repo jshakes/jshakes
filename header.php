424e5550e0bad25083eabbd976852bb9d9db7af2
James Shakespeare <j@jshakespeare.com>
2013-02-05 00:31:16 +0000

    analytics

--- header.php
+++ header.php
@@ -29,6 +29,19 @@
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('title'); ?> RSS Feed" href="/feed/" />
 
 	<?php wp_head(); ?>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-6245948-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
 </head>
 <body id="<?php echo $post->post_name; ?>" <?php echo body_class(); ?>>
 
